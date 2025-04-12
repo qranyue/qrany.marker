8186ee2f966cafd0556aaba69878af5088f230db
qranyue <qran.yue@hotmail.com>
2025-04-12 20:36:21 +0800

    feat(Program.cs): 添加WebApi外部HTTP端点支持

--- Marker.AppHost/Program.cs
+++ Marker.AppHost/Program.cs
@@ -3,6 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var redis = builder.AddRedis("redis");
 var mongo = builder.AddMongoDB("mongo").WithDataVolume().AddDatabase("marker");
 
-builder.AddProject<Projects.Marker_WebApi>("webapi").WithReference(redis).WithReference(mongo);
+builder.AddProject<Projects.Marker_WebApi>("webapi").WithExternalHttpEndpoints().WithReference(redis).WithReference(mongo);
 
 builder.Build().Run();
